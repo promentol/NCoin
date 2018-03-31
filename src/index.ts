4932bf96f1c959db18653ce92059910bd0ddb909
Narek <promentol@gmail.com>
2018-03-31 21:21:12 +0400

    command line arguments

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,57 @@
-//data
-//network port
+///<reference path="../node_modules/@types/node/index.d.ts" />
+
+import * as BlockChain from './core'
+import {
+    initREST
+} from './rest'
+
+//PROCESS ARGUMENTS
+const parseArgs = require('minimist')
+const args = parseArgs(process.argv)
+const fs = require('fs')
+
+
+//data directory
+const dataDirectory = args.d || args.data || '../data'
+
+try {
+    const stats = fs.lstatSync(dataDirectory)
+
+    if (!stats.isDirectory()) {
+        console.error(`${dataDirectory} is not a directory`);
+        process.exit()
+    }
+} catch(e) {
+    if (e.code == 'ENOENT') {
+        console.error(`${dataDirectory} is not a directory`);
+    } else {
+        console.error(e)
+    }
+    process.exit()
+}
+
 //private key
-//miner
-//rest port
-
-
 //private key, public key initioation
+const keyDirectory = args.k || args.key
+const key = keyDirectory ? fs.readFileSync(keyDirectory) : BlockChain.generatePrivateKey()
 
+if (!BlockChain.verifyPrivateKey(key)) {
+    console.error(`Not valid private key`);
+    process.exit()
+}
+
+
+//network port
+//initialize network
 //initialize Network
+
+//rest port
+//initialize REST API
+const restPort = parseInt(args.r) || parseInt(args.rest)
+if (restPort) {
+    initREST(restPort)
+}
+
+//miner
 //initialize Miner Network
 
-//initialize REST API
